a3b7ee296db609bff99f853b71d8e0e7b9285c5e
Thiago Rocha <silva.throcha@gmail.com>
2024-05-02 21:38:14 -0300

    .

--- forca.py
+++ forca.py
@@ -1,52 +1,86 @@
+def mostrar_forca(chave, tentativa):
+    forca = {
+    'nv1': '------------',
+    'nv2': '|',
+    'nv3': '|',
+    'nv4': '|',
+    'nv5': '|',
+    'nv6': 'palavra chave: '
+    }
+    
+    if tentativa == '':
+        forca['nv6'] += ''.join(['_']*len(chave))
+    else: forca['nv6'] = modificar_nv6(chave, forca, tentativa)
 
+    for i in forca:
+        print(forca[i])
+
+def modificar_nv6(chave, forca, tentativa):
+    posicoes = []
+    qntd_letras = len(chave)
+    
+    if tentativa in chave:
+        for i in range(len(chave)):
+            if chave[i] == tentativa:
+                posicoes.append(i)
+    # for posicao in posicoes:
+    #     for 
+# def mostrar(tentativa, certas):
+#     if tentativa == 'certo':
+#         # print(f'A letra {tentativa} está certa. {certas}')
+#     elif tentativa == 'errado':
+#         print(f'A letra {tentativa} não faz parte da palavra chave')
+    
 def pedir_jogada():
     jogada = input('Tente uma letra ou a palavra chave > ').upper()
     return jogada
 
 def verificador(tentativa):
     if tentativa == '' or type(tentativa) != str:
-        validado = False
-        return validado
-    elif len(tentativa) > 1:
-        validado = 'Chute'
-        return validado
-    else: 
-        validado = True
-        return validado
+        return False
+    else: return True
         
-def tentativa_keyword(tentativa, keyword, continuar):
+def tentativa_final(tentativa, keyword, continuar):
     if tentativa == keyword:
         print("Acertou!")
-        continuar = False
-        return continuar
+        return False
     else: 
         print("Errou tente novamente")
-        continuar = True
-        return continuar
+        return True
 
-def tentativa_letra(tentativa, realizado_ant, chave, certas):
-    if tentativa in chave:
-        realizado_ant = realizado_ant.append(tentativa)
-        certas = certas.append(tentativa)
-        print(realizado_ant)
-        print(certas)
-
-
+def caminho_tentativa(tentativa, realizadas, certas, chave):
+    if len(tentativa) > 1:
+        return 'final'
+    else:
+        if tentativa not in realizadas:
+            realizadas = realizadas.append(tentativa)
+        else: print('Já foi jogado')
+        
+        if tentativa in chave:
+            if tentativa not in certas:
+                certas = certas.append(tentativa)
+            return 'certo'
+        else: return 'errado'
 
 keyWord = input('Digite a palavra chave > ').upper()
 continue_game = True
-realizado_antes = []
-letras_certas = []
+jogadas_realizadas = []
+jogadas_certas = []
+jogada = ''
 
 while continue_game == True:
+    mostrar_forca(keyWord, jogada)
     jogada = pedir_jogada()
     verificado = verificador(jogada)
     if verificado == False:
         print('Jogada inválida')
-    elif verificado == 'Chute':
-        continue_game = tentativa_keyword(jogada, keyWord, continue_game)
-    elif verificado == True:
-        jogada_validada = tentativa_letra(jogada, realizado_antes, keyWord, letras_certas)
+    else: 
+        tentativa = caminho_tentativa(jogada, jogadas_realizadas, jogadas_certas, keyWord)
+        if tentativa == 'final':
+            continue_game = tentativa_final(jogada, keyWord, continue_game)
+        # else:
+        #     mostrar(tentativa, jogadas_certas)
+            
     
 
 
--- teste.py
+++ teste.py
@@ -3,31 +3,76 @@
 # print('|       / | \ ')
 # print("|        / \ ")
 # print('|')
+# forca = {
+#     'cima': '----------',
+#     'meio': '|',
+#     'baixo': '|'
+# }
 
+# for i in forca:
+#     print(forca[i])
+# ei = input()
+
+# if ei == '1':
+#     forca['meio'] += '        0'
+
+# for i in forca:
+#     print(forca[i])
 # print('-----------')
+
+
 # print('|         ')
 # print('|        ')
 # print("|         ")
 # print('|')
 
-def pao(ei, op):
-    if ei not in op:
-        op = op.append(ei)
+# def pao(ei, op):
+#     if ei not in op:
+#         op = op.append(ei)
 
     
+# opa = []
+# ei = input('teste > ')
+# key = 'labio'
 
-ei = input('teste > ')
-key = 'labio'
-opa = []
+# while ei != 'ok':
+#     if ei in key:
+#         po = pao(ei, opa)
+#         ei = input('teste > ')   
+#     else: 
+#         print('não ta')
+#         ei = input('teste > ')
+# print(opa)
 
-while ei != 'ok':
-    if ei in key:
-        po = pao(ei, opa)
-        ei = input('teste > ')  
-        print(opa)
-    else: print('não ta')
+# def soma(ei):
+#     ei += 1 
+#     return ei
 
+# ei = 0
 
+# pao = soma(ei)
 
+# print(ei)
 
+# key = 'apa'
+# ei = input()
 
+# if ei in key:
+#     print(key.index(ei))
+
+ei = input()
+key = 'tomou'
+posicoes = []
+tamanho = len(key)
+escondido = ['_']*tamanho
+print(''.join(escondido))
+
+for i in range(len(key)):
+    if key[i] == ei:
+        posicoes.append(i)
+print(posicoes)
+
+if ei in key:
+    for posicao in posicoes:
+        escondido[posicao] = ei
+print(''.join(escondido))
